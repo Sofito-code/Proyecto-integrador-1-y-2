bc0cb1b74b2a94d8978adc5fc03e0e9a55945b12
Daniel Tabares <daniel.tabares1@udea.edu.co>
2023-04-25 23:53:40 -0500

    Agrego modelo y scripts del cambio de escena

---
+++ Assets/Scripts/Navegacion/LevelLoader.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class LevelLoader
+{
+    public static string nextLevel;
+
+    public static void LoadLevel(string levelName)
+    {
+        nextLevel = levelName;
+        SceneManager.LoadScene("Loading");
+    }
+}
