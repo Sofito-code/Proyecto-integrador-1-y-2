d289a949d005481beb3f8dfcc811e00dfede45d3
Sofito-code <65092315+Sofito-code@users.noreply.github.com>
2023-05-10 12:07:12 -0500

    Randomización de preguntas

---
+++ Assets/Scripts/Juego Memoria/CardInfo.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardInfo
+{
+    public string level { get; }
+    public string id { get; }
+    public string question { get; }
+    public string answer { get; }
+    public int topic { get; }
+
+    public CardInfo() { }
+
+    public CardInfo(string level, string id, string question, string answer, int topic)
+    {
+        this.level = level;
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+        this.topic = topic;
+    }
+}
