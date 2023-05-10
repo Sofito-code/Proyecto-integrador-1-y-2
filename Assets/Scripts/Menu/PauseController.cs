69770b2afe0bf042cb63d0cbd4915d9fe1b4aa42
Sofito-code <65092315+Sofito-code@users.noreply.github.com>
2023-05-10 01:18:49 -0500

    Bloque de salida y Pausar el juego

--- Assets/Scripts/Menu/PauseController.cs
+++ Assets/Scripts/Menu/PauseController.cs
@@ -7,31 +7,37 @@ public class PauseController : MonoBehaviour
     public static bool gameisPaused = false;
     public GameObject pauseMenuUI;
 
-        void Update(){
-        if(Input.GetKeyDown(KeyCode.Escape)){
-
-            if(gameisPaused){
-                Resume();
-            }
-
-            else{
-                Pause();
-            }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Action();
         }
     }
 
-    public void Resume(){
-        pauseMenuUI.SetActive(false);
-         Time.timeScale = 1f;
-         gameisPaused = false;
-
+    public void Action()
+    {
+        if (gameisPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
     }
 
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameisPaused = false;
+    }
 
-    void Pause(){
+    void Pause()
+    {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         gameisPaused = true;
-
     }
 }
