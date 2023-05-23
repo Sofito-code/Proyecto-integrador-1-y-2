14ee09718adf6871c9ee85afa593c48cf6e52b1d
Sofito-code <65092315+Sofito-code@users.noreply.github.com>
2023-05-22 23:32:09 -0500

    Configurar preguntas en la pista

---
+++ Assets/Scripts/Juego Correr/Question.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Question : MonoBehaviour
+{
+    public GameObject title;
+    public GameObject answer1;
+    public GameObject answer2;
+}
