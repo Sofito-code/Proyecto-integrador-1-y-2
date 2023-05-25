3d23d44fa423e21d141f183a0e976479456c6a10
Sofito-code <65092315+Sofito-code@users.noreply.github.com>
2023-05-25 03:28:53 -0500

    Final del juego

--- Assets/Scripts/Juego Carrera/QuestionController.cs
+++ Assets/Scripts/Juego Carrera/QuestionController.cs
@@ -1,9 +1,9 @@
-using System.Globalization;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.IO;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine;
 using TMPro;
 
 public class QuestionController : MonoBehaviour
@@ -24,21 +24,30 @@ public class QuestionController : MonoBehaviour
         { 65.323f, 26.01f, 60.628f }
     };
 
+    public GameObject player;
     public GameObject questionPrefab;
     public GameObject instructions;
     public GameObject gameInfoInf;
     public GameObject gameInfoSup;
-    public GameObject pause;
+    public GameObject pauseCanvas;
+    public GameObject gameOverCanvas;
+    public GameObject start;
     public Transform questionsParent;
-    private int success = 0;
-    private int score = 0;
-    private int rate = 0;
+    public Sprite[] pauseSprites;
     public string level;
 
+    private int successes = 0;
+    private int scoring = 0;
+    private int bonusScore = 0;
+    private int rate = 0;
+    private int questionCheck = 0;
+    private int wrongAnswers = 0;
+    private int streakCounter = 0;
     private List<QuestionInfo> questions;
     private List<GameObject> modules = new List<GameObject>();
     private bool playing = false;
     private static bool gameisPaused = false;
+    private Coroutine scoreC;
 
     // Start is called before the first frame update
     void Start()
@@ -51,63 +60,30 @@ public class QuestionController : MonoBehaviour
     {
         if (playing)
         {
-            /*             if(temp>= limit || remainingClicks == 0 || successesCards == 7){
-                            GameOver();
-                        }
-                        UploadTimeUI();  */
-            //UploadUI();
-            
-            StartCoroutine(Score());
-        }
-        
-    }
-
-    IEnumerator Score()
-    {
-        bool rate = false;
-        int scores = 0;
-        int succ = 0;
-        int rateConsec = 0;
-        int rates = 0;
-        yield return new WaitForSeconds(Time.deltaTime);
-        for (int i = 0; i < modules.Count; i++)
-        {            
-            if (modules[i].GetComponent<Question>().answered) {
-                
-                if(!rate){                    
-                    rateConsec = 0;
+            scoreC = StartCoroutine(Score());
+            if (12 == questionCheck || 5 == wrongAnswers)
+            {
+                GameOver();
+                if(questionCheck == 12){
+                    this.GetComponent<RunnerController>().StopSpawn();
+                    StopCoroutine(scoreC);     
+                    VictoryAnimate();
                 }
-                if(modules[i].GetComponent<Question>().isChoseCorrect){
-                    rate = true;
-                    rateConsec += 1;
-                    succ += 1;
-                }
-                else{
-                    rate = false;
-                }
-                if(rateConsec == 4){
-                    rates += 1;
-                    rateConsec = 0;
+                if(wrongAnswers == 5){ 
+                    this.GetComponent<RunnerController>().StopSpawn();
+                    StopCoroutine(scoreC);            
+                    DefeatAnimate();
                 }
             }
         }
-        scores = succ * 30;
-        this.success = succ;
-        this.score =  scores;
-        this.rate = rates;
-        UploadUI();
-        //Debug.Log($"ACIERTOS: {success} PUNTAJE: {score} RACHA DE ACIERTOS: {rates}");
-        
+    }    
+
+    void VictoryAnimate(){
+        player.transform.GetChild(0).GetComponent<Animator>().SetBool("Victory", true);
     }
 
-    public void UploadUI()
-    {
-        gameInfoInf.transform.GetChild(3).gameObject.GetComponent<TMP_Text>().text =
-            "RACHA DE ACIERTOS: " + rate;
-        gameInfoInf.transform.GetChild(1).gameObject.GetComponent<TMP_Text>().text =
-            "ACIERTOS: " + success;
-        gameInfoSup.transform.GetChild(3).gameObject.GetComponent<TMP_Text>().text =
-            "PUNTAJE: " + score;
+    void DefeatAnimate(){
+        player.transform.GetChild(0).GetComponent<Animator>().SetBool("Defeat", true);
     }
 
     public void Create()
@@ -132,15 +108,103 @@ public class QuestionController : MonoBehaviour
     {
         for (int i = 0; i < modules.Count; i++)
         {
-            modules[i].GetComponent<Question>().title.GetComponent<TMPro.TextMeshPro>().text =
-                questions[i].question;
-            modules[i].GetComponent<Question>().answer1.GetComponent<TMPro.TextMeshPro>().text =
-                questions[i].answer1;
-            modules[i].GetComponent<Question>().answer2.GetComponent<TMPro.TextMeshPro>().text =
-                questions[i].answer2;
-            modules[i].GetComponent<Question>().id = questions[i].id;
-            modules[i].GetComponent<Question>().rightAnswer = questions[i].rightAnswer;
+            Question question = modules[i].GetComponent<Question>();
+            question.title.GetComponent<TMPro.TextMeshPro>().text = questions[i].question;
+            question.answer1.GetComponent<TMPro.TextMeshPro>().text = questions[i].answer1;
+            question.answer2.GetComponent<TMPro.TextMeshPro>().text = questions[i].answer2;
+            question.id = questions[i].id;
+            question.rightAnswer = questions[i].rightAnswer;
         }
+    }
+
+    IEnumerator Score()
+    {
+        streakCounter = 0;
+        bool isStreak = false;
+        int wAnswers = 0;
+        int score = 0;
+        int success = 0;
+        int streak = 0;
+        int qCheck = 0;
+        yield return new WaitForSeconds(Time.deltaTime);
+        for (int i = 0; i < modules.Count; i++)
+        {
+            if (modules[i].GetComponent<Question>().answered)
+            {
+                qCheck += 1;
+                if (!isStreak)
+                {
+                    streakCounter = 0;
+                }
+                if (modules[i].GetComponent<Question>().isChoseCorrect)
+                {
+                    isStreak = true;
+                    streakCounter += 1;
+                    success += 1;
+                }
+                else
+                {
+                    wAnswers += 1;
+                    isStreak = false;
+                }
+                if (streakCounter == 4)
+                {
+                    streak += 1;
+                    streakCounter = 0;
+                }
+            }
+        }
+        questionCheck = qCheck;
+        wrongAnswers = wAnswers;
+        score = success * 30;
+        bonusScore = streak * 15;
+        this.successes = success;
+        this.scoring = score;
+        this.rate = streak;
+        gameInfoInf.transform.GetChild(3).gameObject.GetComponent<TMP_Text>().text =
+            "RACHA DE ACIERTOS: " + rate;
+        gameInfoInf.transform.GetChild(1).gameObject.GetComponent<TMP_Text>().text =
+            "ACIERTOS: " + successes;
+        gameInfoSup.transform.GetChild(3).gameObject.GetComponent<TMP_Text>().text =
+            "PUNTAJE: " + scoring;
+    }  
+
+    public void GameOver(){        
+        //cardsParentObject.SetActive(false);
+        gameInfoSup.SetActive(false);
+        gameInfoInf.SetActive(false);
+        gameOverCanvas.transform.GetChild(1).gameObject.GetComponent<TMP_Text>().text = "PUNTAJE: " + scoring;
+        gameOverCanvas.transform.GetChild(2).gameObject.GetComponent<TMP_Text>().text = "BONUS: " + bonusScore;
+        gameOverCanvas.transform.GetChild(3).gameObject.GetComponent<TMP_Text>().text = "TOTAL: " + (scoring + bonusScore);
+        gameOverCanvas.SetActive(true); 
+        playing = false;    
+    }
+
+    public void ReloadGame(){
+        //set en cero todo
+        successes = 0;
+        scoring = 0;
+        bonusScore = 0;
+        rate = 0;
+        questionCheck = 0;
+        wrongAnswers = 0;
+        streakCounter = 0;
+        playing = true;
+        instructions.SetActive(true);
+        gameInfoInf.SetActive(false);
+        gameInfoSup.SetActive(false);
+        questionsParent.gameObject.SetActive(false);    
+        gameOverCanvas.SetActive(false);  
+        player.transform.GetChild(0).GetComponent<Animator>().SetBool("Defeat", false);    
+        player.transform.GetChild(0).GetComponent<Animator>().SetBool("Victory", false);    
+        player.transform.position = new Vector3(63.9f,0.2818546f, 34.5f);
+        player.transform.rotation = Quaternion.Euler(0,-90,0);
+        this.GetComponent<RunnerController>().StopSpawn();        
+        //Eliminar cartas
+        modules.Clear();
+        for (int i = 0; i < questionsParent.childCount; ++i)
+            Destroy(questionsParent.GetChild(i).gameObject);       
+        Start();
     }
 
     public void StartGame()
@@ -150,6 +214,7 @@ public class QuestionController : MonoBehaviour
         gameInfoSup.SetActive(true);
         questionsParent.gameObject.SetActive(true);
         playing = true;
+        
     }
 
     public void PauseGame()
@@ -166,8 +231,9 @@ public class QuestionController : MonoBehaviour
 
     public void Resume()
     {
-        //gameInfoInf.transform.GetChild(5).gameObject.GetComponent<Button>().image.sprite= pauseSprites[0];
-        pause.SetActive(false);
+        gameInfoInf.transform.GetChild(5).gameObject.GetComponent<Button>().image.sprite =
+            pauseSprites[0];
+        pauseCanvas.SetActive(false);
         Time.timeScale = 1f;
         gameisPaused = false;
         playing = true;
@@ -175,8 +241,9 @@ public class QuestionController : MonoBehaviour
 
     void Pause()
     {
-        //gameInfoInf.transform.GetChild(5).gameObject.GetComponent<Button>().image.sprite= pauseSprites[1];
-        pause.SetActive(true);
+        gameInfoInf.transform.GetChild(5).gameObject.GetComponent<Button>().image.sprite =
+            pauseSprites[1];
+        pauseCanvas.SetActive(true);
         Time.timeScale = 0f;
         gameisPaused = true;
         playing = false;
