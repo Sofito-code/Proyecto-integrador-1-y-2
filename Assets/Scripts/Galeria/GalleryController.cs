565fe33b35fee7070275f8dc6603b5dae84c938c
Sofito-code <65092315+Sofito-code@users.noreply.github.com>
2023-06-05 15:53:59 -0500

    Mostrar en la galería las postales

---
+++ Assets/Scripts/Galeria/GalleryController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GalleryController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    List<int> posts;
+    void Start()
+    {
+        posts = this.GetComponent<DBManagement>().QueryGetPaintsAvilable();
+        for (int i = 0; i < posts.Count; i++)
+        {
+            int id = posts[i];
+            Sprite sp  = Resources.Load<Sprite>(id+"");
+            this.transform.GetChild(id).GetChild(0).gameObject.GetComponent<Image>().sprite = sp;
+        }        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
