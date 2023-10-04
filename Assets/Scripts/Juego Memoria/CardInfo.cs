bbe4ccdd5c2bc5da791f9deeeaca088c8e12103e
Sofito-code <65092315+Sofito-code@users.noreply.github.com>
2023-10-04 02:24:25 -0500

    Manejo de preguntas por Json

--- Assets/Scripts/Juego Memoria/CardInfo.cs
+++ Assets/Scripts/Juego Memoria/CardInfo.cs
@@ -5,13 +5,14 @@ using System;
 [Serializable]
 public class CardInfo
 {
-    public string level { get; }
-    public string id { get; }
-    public string question { get; }
-    public string answer { get; }
-    public int topic { get; }
+    public string level;
+    public string id;
+    public string question;
+    public string answer;
+    public int topic;
 
-    public CardInfo() { }
+    public CardInfo() { 
+    }
 
     public CardInfo(string level, string id, string question, string answer, int topic)
     {
